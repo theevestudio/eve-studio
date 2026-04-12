92d8c318ded940287091f895754baeede04500f7
Malia Alana <maliaalana@Malias-MacBook-Air.local>
2026-04-12 11:00:03 -0400

    Fix NDA send: use public URL instead of fs read

--- src/lib/dropbox-sign.ts
+++ src/lib/dropbox-sign.ts
@@ -1,26 +1,24 @@
-import fs from 'fs'
-import path from 'path'
-
 export async function sendNdaForSigning(name: string, email: string, applicationId: string): Promise<string> {
-  const ndaPath = path.join(process.cwd(), 'public', 'E.V.E._studio__beta_tester_nda.pdf')
-  const ndaBuffer = fs.readFileSync(ndaPath)
-  const ndaBlob = new Blob([ndaBuffer], { type: 'application/pdf' })
+  const ndaUrl = 'https://theevestudio.io/E.V.E._studio__beta_tester_nda.pdf'
 
-  const formData = new FormData()
-  formData.append('title', 'E.V.E. Studio Beta Tester NDA')
-  formData.append('subject', 'Sign your E.V.E. Studio Beta NDA')
-  formData.append('message', `Hi ${name}, your beta application has been approved! Please sign the NDA below to activate your access to E.V.E. Studio.`)
-  formData.append('signers[0][email_address]', email)
-  formData.append('signers[0][name]', name)
-  formData.append('files[0]', ndaBlob, 'eve-studio-beta-nda.pdf')
-  formData.append('metadata[application_id]', applicationId)
+  const params = new URLSearchParams()
+  params.append('title', 'E.V.E. Studio Beta Tester NDA')
+  params.append('subject', 'Sign your E.V.E. Studio Beta NDA')
+  params.append('message', `Hi ${name}, your beta application has been approved! Please sign the NDA below to activate your access to E.V.E. Studio.`)
+  params.append('signers[0][email_address]', email)
+  params.append('signers[0][name]', name)
+  params.append('file_urls[0]', ndaUrl)
+  params.append('metadata[application_id]', applicationId)
 
   const auth = Buffer.from(`${process.env.DROPBOX_SIGN_API_KEY}:`).toString('base64')
 
   const res = await fetch('https://api.hellosign.com/v3/signature_request/send', {
     method: 'POST',
-    headers: { 'Authorization': `Basic ${auth}` },
-    body: formData,
+    headers: {
+      'Authorization': `Basic ${auth}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: params.toString(),
   })
 
   if (!res.ok) {
